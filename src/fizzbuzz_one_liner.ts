6bfcf0f94b1b9d6fb26c10e93b4a9bc8ecf72dac
takashi247 <takashi247@gmail.com>
2022-09-14 04:30:34 +0900

    add fizzbuzz practice scripts

---
+++ src/fizzbuzz_one_liner.ts
@@ -0,0 +1,18 @@
+let line: string = "";
+
+for (let i = 1; i <= 100; ++i) {
+  if (i !== 1) {
+    line += " ";
+  }
+  if (i % 3 === 0 && i % 5 === 0) {
+    line += "FizzBuzz";
+  } else if (i % 3 === 0) {
+    line += "Fizz";
+  } else if (i % 5 === 0) {
+    line += "Buzz";
+  } else {
+    line += String(i);
+  }
+}
+
+console.log(line);
